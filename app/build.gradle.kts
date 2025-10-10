4c5acb556ef906748279a4d97026228e219e1b51
Francis Li <mail@francisli.com>
2025-10-10 11:05:51 -0700

    Match sdk versions to atak project

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
 
     defaultConfig {
         applicationId = "net.peakresponse.android.example"
-        minSdk = 24
+        minSdk = 23
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
