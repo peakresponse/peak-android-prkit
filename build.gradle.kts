5eb7cbf591df8dbc4baffe2bbddee5a58ab92622
Francis Li <mail@francisli.com>
2025-10-10 11:01:35 -0700

    Configure project group

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,8 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+allprojects {
+    group = "net.peakresponse.android.shared"
+}
+
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
